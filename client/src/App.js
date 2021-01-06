f56d7e32ffaf73345ca1d0f6f00bc77fcbaba3ec
c-javier <superqueerhere@gmail.com>
2021-01-06 15:55:24 -0800

    First Commit: Express and React App created

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const hitBackend = () => {
+    axios.get("/test").then((response) => {
+      console.log(response.data);
+    });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={hitBackend}>Send request</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
